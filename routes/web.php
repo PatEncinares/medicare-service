6b0b4beabd5d9b302bcfd586bf0195e05bb97df6
Patrick Encinares <patrickjoseph.greenapple@gmail.com>
2025-07-17 13:55:17 +0800

    FEAT : MEDICAL SERVICE PART C

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\Home;
 use App\Livewire\MedicareServicePartA;
 use App\Livewire\MedicareServicePartB;
+use App\Livewire\MedicareServicePartC;
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', Home::class)->name('home');
 Route::get('/medicare-service-part-a', MedicareServicePartA::class)->name('medicare-service-part-a');
 Route::get('/medicare-service-part-b', MedicareServicePartB::class)->name('medicare-service-part-b');
-
+Route::get('/medicare-service-part-c', MedicareServicePartC::class)->name('medicare-service-part-c');
+// Route::get('/medicare-service-part-d', MedicareServicePartC::class)->name('medicare-service-part-d');
 // Route::get('/', function () {
 //     return view('welcome');
 // })->name('home');
